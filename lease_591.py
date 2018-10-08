bdbbc53db69b3e48a4ade078024cdca96a86563c
a22057916w <a22057916w@gmail.com>
2018-10-09 00:22:39 +0800

    Update lease_591.py

--- lease_591.py
+++ lease_591.py
@@ -6,7 +6,7 @@ import re
 import urllib.request
 import json
 
-LEASE_URL = "https://business.591.com.tw/home/search/rsList?is_new_list=1&storeType=1&type=1&kind=5&searchtype=1&region=1&firstRow=30&totalRows=2797"
+LEASE_URL = "https://business.591.com.tw/home/search/rsList?is_new_list=1&storeType=1&type=1&kind=5&searchtype=1&region=1&firstRow=0&totalRows=2775"
 
 def get_web_page(url):
     time.sleep(0.5)
@@ -16,19 +16,20 @@ def get_web_page(url):
         return None
     else:
         data = json.loads(resp.text)
-        print(data)
-        print(data.keys())
-        print(data["data"])
-        print(data["data"].keys())
-        print(data["data"]["topData"])
-        print("******************************************************************************************************")
-        print(type(data))
-        print(type(data["data"]))
-        print(type(data["data"]["topData"]))
+        #print(data)
+        #print(data.keys())
+        #print(data["data"])
+        #print(data["data"].keys())
+        #print(data["data"]["topData"])
+        #print("******************************************************************************************************")
+        #print(type(data))
+        #print(type(data["data"]))
+        #print(type(data["data"]["topData"]))
         print(data["data"]["topData"][0].keys())
         d = data["data"]["topData"][0]
         print(type(d))
         print(d.items())
+        print(data["data"]["data"][0].items())
         """for d in data["data"]["topData"]:
             print(d.keys())"""
         return resp.text
