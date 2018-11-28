bfa6eb919ad677030183397e04086631b4eda00a
a22057916w <a22057916w@gmail.com>
2018-11-28 23:26:22 +0800

    test

--- myio.py
+++ myio.py
@@ -1,18 +1,18 @@
-def read_excel():
+def read_excel(fname):
     try:
-        df = pd.read_excel("lease/data/total_rows_NTC.xlsx")
+        df = pd.read_excel(fname)
         my_dict = df.to_dict("records")
         return my_dict  # return a list of dict
 
     except Exception as e:
         print(e)
 
-def save(data):
+def save(data, fname):
 
     df = pd.DataFrame.from_dict(data)
-    writer = pd.ExcelWriter('lease/data/loc_NTC.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(fname + ".xlsx", engine='xlsxwriter')
     df.to_excel(writer, sheet_name='lease_NTC_location')
     writer.save()
 
-    with open('lease/data/loc_NTC.json', 'w', encoding='utf-8') as f:
+    with open(fname + ".json", 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, sort_keys=True, ensure_ascii=False)
--- test.py
+++ test.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     dir = "D:/Python/database/test"
 
     os.makedirs(dir)
+"lease/data/total_rows_NTC.xlsx"
