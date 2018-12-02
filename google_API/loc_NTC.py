cc678eafc9b39ab7901e31fe328d450ca25207c6
a22057916w <a22057916w@gmail.com>
2018-12-02 20:31:29 +0800

    update loc

--- google_API/loc_NTC.py
+++ google_API/loc_NTC.py
@@ -4,11 +4,9 @@ sys.path.append("script/")
 from datetime import datetime
 from myio import read_excel, save
 
-if __name__ == "__main__":
-    row_data = read_excel("lease/data/total_rows_NTC.xlsx")
-
-    gmaps = googlemaps.Client(key = "AIzaSyDcixkMKgROY2tE_4VLPTioPtDOwbmzfcI")
+def get_geoLoc(row_data):
     geo_result = []
+
     for data in row_data:
         coordinate = gmaps.geocode(data["addr"])[0]["geometry"]["location"]
         print(data["addr"] + ":", coordinate)
@@ -16,6 +14,16 @@ if __name__ == "__main__":
             "addr": data["addr"],
             "coordinate": coordinate
         })
-    print(geo_result)
+    return geo_result
 
+if __name__ == "__main__":
+    gmaps = googlemaps.Client(key = "AIzaSyDcixkMKgROY2tE_4VLPTioPtDOwbmzfcI")
+
+    # data for lease NTC
+    row_data = read_excel("lease/data/total_rows_NTC.xlsx")
+    geo_result = get_geoLoc(row_data)
     save(geo_result, "lease/data/loc_NTC")
+    # data for sells NTC
+    row_data = read_excel("sells/data/total_rows_NTC.xlsx")
+    geo_result = get_geoLoc(row_data)
+    save(geo_result, "sells/data/loc_NTC")
